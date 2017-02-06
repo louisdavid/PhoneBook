49f8ac2cbc8f8468a9dd270446c65b4bd4ce3d92
Cote <1348856@P322-28.local>
2017-02-06 14:43:14 -0500

    PhoneBook - Initial
    Initial Commit

--- PhoneBook/ViewController.swift
+++ PhoneBook/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    let theList = ["Hello", "My Name is Jack Burns"]
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.theList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let theCell = self.tableView.dequeueReusableCell(withIdentifier: "TheCell", for: indexPath)
+        theCell.textLabel?.text = self.theList[indexPath.row]
+        theCell.detailTextLabel?.text = "Phone Number will go here"
+        return theCell
+    }
+    
+    
+    @IBAction func doAdd(_ sender: UIBarButtonItem) {
+    }
 }
 
